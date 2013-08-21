510fbaadcbdb66ff64f4d9e31bc29a089a1a8c49
Michael Sokol <mikaa123@gmail.com>
2013-08-21 16:10:36 +0200

    adding a factory to create appropriate dictionaries.

--- models/dictionary_properties.js
+++ models/dictionary_properties.js
@@ -1,3 +1,12 @@
 var Dictionary = require('./dictionary');
 
-module.exports = Dictionary.extend({});
+module.exports = Dictionary.extend({
+	defaults: {
+		type: 'properties',
+		selected: true
+	},
+
+	initialize: function(options) {
+		console.log('new properties');
+	}
+});
--- models/dictionary_xml.js
+++ models/dictionary_xml.js
@@ -1,3 +1,12 @@
 var Dictionary = require('./dictionary');
 
-module.exports = Dictionary.extend({});
+module.exports = Dictionary.extend({
+	defaults: {
+		type: 'xml',
+		selected: true
+	},
+
+	initialize: function(options) {
+		console.log('new xml');
+	}
+});
