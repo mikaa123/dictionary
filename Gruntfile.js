7c6b8303a6288ecfebfc6bcfd9b63624af7e808f
Michael Sokol <mikaa123@gmail.com>
2013-08-22 16:11:30 +0200

    working on grunt task to automate building a package for the project.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,34 +1,33 @@
 module.exports = function(grunt) {
-  var exec = require('child_process').exec;
+	var exec = require('child_process').exec;
 
-  // Project configuration.
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
-    sass: {
-      dist: {
-        files: {
-          'css/base.css': 'css/base.scss'
-        }
-      }
-    },
+	// Project configuration.
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json')
+	});
 
-    watch: {
-      reload: {
-        files: 'index.html',
-      },
-      scss: {
-        files: 'css/base.scss',
-        tasks: 'sass'
-      }
-    }
-  });
+	grunt.registerTask('package', 'Package the app for distribution.', function(platform) {
+		// var done = this.async(),
+		// 	nwBinary = {
+		// 		linux: 'https://s3.amazonaws.com/node-webkit/v0.7.1/node-webkit-v0.7.1-linux-x64.tar.gz'
+		// 	};
 
-  grunt.event.on('watch', function(action, path, target) {
-    if (target === 'reload') {
-      // exec('nw .');
-    }
-  });
-
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-contrib-sass');
+		// grunt.log.writeln('Downloading the latest node-webkit binary for Linux...');
+		// exec('wget ' + nwBinary.linux + ' -O bin/temp', function(error, stdout, stderr) {
+		// 	if (!error) {
+		// 		grunt.log.writeln('Extracting the archive into bin/tmp...');
+		// 		exec('mkdir bin/tmp; tar xf bin/temp -C bin/tmp; rm bin/temp', function(error, stdout, stderr) {
+		// 			grunt.log.writeln('Creating a .nw package...');
+		// 			// exec('find . -! -name "bin" -print | zip bin/tmp/node-webkit-v0.7.1-linux-x64/package.nw -@', function(error, stdout, stderr) {
+		// 			// 	grunt.log.writeln('Packaging the app into bin/linux');
+		// 			// 	exec('zip bin/tmp/node-webkit-v0.7.1-linux-x64/* bin/linux/dictionary.zip; rm -rf bin/tmp', function(error, stdout, stderr) {
+		// 			// 		done();
+		// 			// 	});
+		// 			// });
+		// 		});
+		// 	} else {
+		// 		done(false);
+		// 	}
+		// });
+	});
 };
