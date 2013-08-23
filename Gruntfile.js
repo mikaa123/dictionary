542d30fda61be415575984376deb325797d1f6f3
Michael Sokol <mikaa123@gmail.com>
2013-08-23 12:12:17 +0200

    Grunt multitask for linux distribution.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,33 +1,46 @@
+var async = require('async');
+
 module.exports = function(grunt) {
 	var exec = require('child_process').exec;
 
 	// Project configuration.
 	grunt.initConfig({
-		pkg: grunt.file.readJSON('package.json')
+		pkg: grunt.file.readJSON('package.json'),
+		distribute: {
+			linux: {
+				binaryUrl: 'https://s3.amazonaws.com/node-webkit/v0.7.1/node-webkit-v0.7.1-linux-x64.tar.gz',
+				extractCommand: 'tar xf bin/temp -C bin/tmp --strip=1'
+			}
+		}
 	});
 
-	grunt.registerTask('package', 'Package the app for distribution.', function(platform) {
-		// var done = this.async(),
-		// 	nwBinary = {
-		// 		linux: 'https://s3.amazonaws.com/node-webkit/v0.7.1/node-webkit-v0.7.1-linux-x64.tar.gz'
-		// 	};
+	grunt.registerMultiTask('distribute', 'Prepares a package for the app.', function() {
+		var done = this.async(),
+			target = this.target,
+			binaryUrl = this.data.binaryUrl,
+			extractCommand = this.data.extractCommand;
 
-		// grunt.log.writeln('Downloading the latest node-webkit binary for Linux...');
-		// exec('wget ' + nwBinary.linux + ' -O bin/temp', function(error, stdout, stderr) {
-		// 	if (!error) {
-		// 		grunt.log.writeln('Extracting the archive into bin/tmp...');
-		// 		exec('mkdir bin/tmp; tar xf bin/temp -C bin/tmp; rm bin/temp', function(error, stdout, stderr) {
-		// 			grunt.log.writeln('Creating a .nw package...');
-		// 			// exec('find . -! -name "bin" -print | zip bin/tmp/node-webkit-v0.7.1-linux-x64/package.nw -@', function(error, stdout, stderr) {
-		// 			// 	grunt.log.writeln('Packaging the app into bin/linux');
-		// 			// 	exec('zip bin/tmp/node-webkit-v0.7.1-linux-x64/* bin/linux/dictionary.zip; rm -rf bin/tmp', function(error, stdout, stderr) {
-		// 			// 		done();
-		// 			// 	});
-		// 			// });
-		// 		});
-		// 	} else {
-		// 		done(false);
-		// 	}
-		// });
+		async.series([
+			function(callback) {
+				grunt.log.writeln('Downloading the latest node-webkit binary for ' + target + '...');
+				exec('wget ' + binaryUrl + ' -O bin/temp', callback);
+			},
+			function(callback) {
+				grunt.log.writeln('Extracting archive...');
+				exec('mkdir bin/tmp; ' + extractCommand + '; rm bin/temp', callback);
+			},
+			function(callback) {
+				grunt.log.writeln('Creating package for the app...');
+				exec('zip -q -r bin/tmp/package.nw * -x "*bin*" -x "*.git*"', callback);
+			},
+			function(callback) {
+				grunt.log.writeln('Creating zip file for ' + target + '...');
+				exec('zip -q -r -j bin/' + target + '/dictionary.zip bin/tmp/*; rm -rf bin/tmp', callback);
+			}
+		],
+		function(err, results) {
+			if (err) grunt.log.error(err);
+			done();
+		});
 	});
 };
